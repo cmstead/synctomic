735b5217d89447f5032922aed09bfd186ec30a0e
Chris Stead <cmstead@gmail.com>
2017-07-21 16:59:39 -0700

    Added setOnce and onChange

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
     let isNode = typeof module !== undefined && typeof module.exports !== undefined
 
     if (isNode) {
-        const signet = require('./signet-types');
+        const signet = require('signet')();
         const matchlight = require('matchlight')(signet);
 
         module.exports = moduleFactory(signet, matchlight.match);
@@ -12,60 +12,213 @@
         throw new Error('The module synctomic requires Signet to run.');
     }
 
-})(function () {
+})(function (signet, match) {
     'use strict';
 
-    return function synctomic(dataValue) {
-        const dataStore = {
-            value: dataValue,
-            updateTimestamp: process.hrtime()
-        };
+    function helperFactory() {
 
-        function fastCopy(object) {
-            return JSON.parse(JSON.stringify(object));
-        }
+        const isNull = signet.isTypeOf('null');
+        const isReferencible = signet.isTypeOf('variant<composite<^null, object>, function>');
 
         function constructTimestamp(timevalues) {
             return timevalues[0] * 1000000 + timevalues[1] / 1000;
         }
 
-        function compareTimes(timevalues1, timevalues2) {
-            var timestamp1 = constructTimestamp(timevalues1);
-            var timestamp2 = constructTimestamp(timevalues2);
-
-            return timestamp1 === timestamp2;
+        function getHRTimestamp() {
+            return constructTimestamp(process.hrtime());
         }
 
-        const updateFactory = (lastTimestamp, mutator) => (updatedData) => {
-            if (compareTimes(dataStore.updateTimestamp, lastTimestamp)) {
-                dataStore.value = updatedData;
-                dataStore.updateTimestamp = process.hrtime();
-            } else {
-                set(mutator);
-            }
-        };
+        function fastCopy(object) {
+            return JSON.parse(JSON.stringify(object));
+        }
+
+        function getValue(data, key) {
+            return match(data, (matchCase, matchDefault) => {
+                matchCase(isReferencible, (data) => data[key]);
+                matchDefault(() => null);
+            });
+        }
+
+        function recurOrReturn(data, keyTokens) {
+            return match(data, (matchCase, matchDefault) => {
+                matchCase(isNull, (data) => data);
+                matchDefault(() => deref(data, keyTokens));
+            });
+        }
+
+        function deref(data, keyTokens) {
+            return match(keyTokens, (matchCase, matchDefault) => {
+                matchCase([], () => data);
+                matchDefault(([key, ...rest]) => recurOrReturn(getValue(data, key), rest));
+            });
+        }
+
+        function getProperty(data, deepKey) {
+            const keyTokens = match(deepKey.trim(), (matchCase, matchDefault) => {
+                matchCase('', () => []);
+                matchDefault(() => {
+                    return deepKey
+                        .split('.')
+                        .map((key) => key.trim());
+                });
+            });
+
+            return deref(data, keyTokens);
+        }
 
         function asynchronizeMutator(mutator) {
-            const mutatorArity = mutator.length;
-            return mutatorArity > 1 ? mutator : (value, update) => update(mutator(value));
+            return mutator.length > 1
+                ? mutator
+                : (value, update) => update(mutator(value));
+        }
+
+        return {
+            asynchronizeMutator: asynchronizeMutator,
+            fastCopy: fastCopy,
+            getHRTimestamp: getHRTimestamp,
+            getProperty: getProperty
+        };
+
+    }
+
+    function watcherFactory(dataStore, { fastCopy }) {
+        let watchers = {};
+
+        function getWatchState(watchCheck) {
+            return JSON.stringify(watchCheck(dataStore.value));
+        }
+
+        function callWatchers(lastData) {
+            const currentData = fastCopy(dataStore.value);
+            const lastDataCopy = fastCopy(lastData);
+
+            Object.keys(watchers)
+                .forEach((key) => {
+                    const watcher = watchers[key];
+                    const currentState = getWatchState(watcher.watchCheck);
+                    const stateMatch = watcher.lastState === currentState;
+
+                    if (!stateMatch) {
+                        watchers[key].watchAction(currentData, lastDataCopy);
+                        watcher.lastState = currentState;
+                    }
+                });
+        }
+
+        function buildWatcher(watchCheck, watchAction) {
+            return {
+                lastState: getWatchState(watchCheck),
+                watchCheck: watchCheck,
+                watchAction: watchAction
+            };
+        }
+
+        function addWatcher(key, watcher) {
+            watchers[key] = watcher;
+        }
+
+        function deleteWatcherFactory(key) {
+            return () => { watchers[key] = undefined; delete watchers[key]; };
+        }
+
+        return {
+            addWatcher: addWatcher,
+            buildWatcher: buildWatcher,
+            callWatchers: callWatchers,
+            deleteWatcherFactory: deleteWatcherFactory
+        };
+    }
+
+    function buildAtom(dataValue) {
+        const helpers = helperFactory();
+        const {
+            asynchronizeMutator,
+            fastCopy,
+            getHRTimestamp,
+            getProperty
+        } = helpers;
+
+        let dataStore = {
+            value: dataValue,
+            updateTimestamp: getHRTimestamp()
+        };
+
+        const watchers = watcherFactory(dataStore, helpers)
+
+        const updateFactory = (lastTimestamp, error) => (updatedData) => {
+            const lastData = fastCopy(dataStore.value);
+            let updateSuccessful = false;
+
+            if (dataStore.updateTimestamp === lastTimestamp) {
+                dataStore.value = updatedData;
+                dataStore.updateTimestamp = getHRTimestamp();
+                updateSuccessful = true;
+
+                watchers.callWatchers(lastData);
+            } else {
+                error();
+            }
+
+            return updateSuccessful;
+        };
+
+        function setter(mutator, error) {
+            const { value, updateTimestamp } = dataStore;
+            const update = updateFactory(updateTimestamp, error);
+
+            asynchronizeMutator(mutator)(fastCopy(value), update);
         }
 
         function set(mutator) {
-            let dataCopy = fastCopy(dataStore.value);
-            const asyncMutator = asynchronizeMutator(mutator);
-            const update = updateFactory(dataStore.updateTimestamp, asyncMutator);
+            const error = () => set(mutator);
+            setter(mutator, error);
+        }
 
-            asyncMutator(dataCopy, update);
+        function setOnce(mutator, error) {
+            const err = typeof error === 'function' ? error : () => null;
+            setter(mutator, err);
         }
 
         function get() {
             return fastCopy(dataStore.value);
         }
 
+        function buildWatchCheck(key) {
+            return (data) => getProperty(data, key);
+        }
+
+        function onChange(watchCheck, watchAction) {
+            const watchKey = JSON.stringify(getHRTimestamp());
+            const constructedCheck = buildWatchCheck(watchCheck);
+            const watcher = watchers.buildWatcher(constructedCheck, watchAction);
+
+            watchers.addWatcher(watchKey, watcher);
+
+            return watchers.deleteWatcherFactory(watchKey);
+        }
+
         return {
-            get: get,
-            set: set
+            get: signet.enforce(
+                '() => *',
+                get),
+            onChange: signet.enforce(
+                'variant<string, function>, ' +
+                'function<* => undefined> ' +
+                '=> function<() => undefined>',
+                onChange),
+            set: signet.enforce(
+                'function<*, [function] => *> ' +
+                '=> undefined',
+                set),
+            setOnce: signet.enforce(
+                'function<*, [function] => *> ' +
+                '=> undefined',
+                setOnce)
         }
     }
+
+    return {
+        buildAtom: buildAtom
+    };
 
 });
--- test/synctomic.test.js
+++ test/synctomic.test.js
@@ -5,8 +5,8 @@ if (typeof exploreFunction !== 'function') {
 }
 
 const assert = require('chai').assert;
-// const sinon = require('sinon');
 const prettyJson = require('./test-utils/prettyJson');
+const sinon = require('sinon');
 
 const synctomic = require('../index.js');
 
@@ -16,7 +16,7 @@ describe('synctomic', function () {
     describe('construction', function () {
         it('should create a new atom when called which stores original data', function () {
             const myData = { foo: 'bar' };
-            const dataAtom = synctomic(myData);
+            const dataAtom = synctomic.buildAtom(myData);
             const result = dataAtom.get();
 
             this.verify(prettyJson(result));
@@ -24,7 +24,7 @@ describe('synctomic', function () {
 
         it('should never provide access to the original object', function () {
             const myData = { foo: 'bar' };
-            const dataAtom = synctomic(myData);
+            const dataAtom = synctomic.buildAtom(myData);
             const result = dataAtom.get();
 
             assert.notEqual(myData, result);
@@ -33,7 +33,7 @@ describe('synctomic', function () {
 
     describe('set', function () {
         it('should expose a set endpoint', function () {
-            const dataAtom = synctomic({ foo: 'bar' });
+            const dataAtom = synctomic.buildAtom({ foo: 'bar' });
             const newData = { baz: 'quux' };
 
             dataAtom.set((data, update) => update(newData));
@@ -43,15 +43,15 @@ describe('synctomic', function () {
         });
 
         it('should iterate on update until update succeeds', function (done) {
-            const dataAtom = synctomic('bar');
+            const dataAtom = synctomic.buildAtom('bar');
             let callCount = 0;
 
             function asyncUpdate(data, update) {
                 setTimeout(function () {
                     callCount++;
-                    update('foo');
+                    const updateSuccessful = update('foo');
 
-                    if (dataAtom.get() === 'foo') {
+                    if (updateSuccessful) {
                         assert.equal(callCount, 2);
                         done();
                     }
@@ -63,12 +63,108 @@ describe('synctomic', function () {
         });
 
         it('should resolve an update on a function which returns a value', function () {
-            const dataAtom = synctomic('foo');
+            const dataAtom = synctomic.buildAtom('foo');
 
             dataAtom.set(() => 'bar');
 
             assert.equal(dataAtom.get(), 'bar');
         });
+    });
+
+    describe('setOnce', function () {
+
+        it('should update like set would without interruption', function () {
+            const dataAtom = synctomic.buildAtom('foo');
+
+            dataAtom.setOnce(() => 'bar');
+
+            assert.equal(dataAtom.get(), 'bar');
+        });
+
+        it('should iterate on update until update succeeds', function (done) {
+            const dataAtom = synctomic.buildAtom('bar');
+
+            function asyncUpdate(data, update) {
+                setTimeout(function () {
+                    update('foo');
+
+                    assert.notEqual(dataAtom.get(), 'foo');
+                    done();
+                }, 10);
+            }
+
+            dataAtom.setOnce(asyncUpdate);
+            dataAtom.set(() => 'baz');
+        });
+
+        it('should should call error on failure', function (done) {
+            const dataAtom = synctomic.buildAtom('bar');
+
+            function asyncUpdate(data, update) {
+                setTimeout(function () {
+                    update('foo');
+                    assert.notEqual(dataAtom.get(), 'foo');
+                }, 10);
+            }
+
+            // This test will fail if done is not called.
+            dataAtom.setOnce(asyncUpdate, done);
+            dataAtom.set(() => 'baz');
+        });
+
+    });
+
+    describe('on', function () {
+
+        it('should watch object and execute when something changes', function () {
+            const dataAtom = synctomic.buildAtom({ foo: { bar: ['baz'] } });
+
+            const watchSpy = sinon.spy();
+            dataAtom.onChange('', watchSpy);
+
+            dataAtom.set((data, update) => {
+                data.foo.bar.push('quux');
+                update(data);
+            });
+
+            this.verify(prettyJson(watchSpy.args));
+        });
+
+        it('should create a deregistration function', function () {
+            const dataAtom = synctomic.buildAtom({ foo: { bar: ['baz'] } });
+
+            const watchSpy = sinon.spy();
+            const deregister = dataAtom.onChange('', watchSpy);
+            deregister();
+
+            dataAtom.set((data, update) => {
+                data.foo.bar.push('quux');
+                update(data);
+            });
+
+            assert.equal(watchSpy.callCount, 0);
+        });
+
+        it('should only fire a watch on a deep property if the property changes', function () {
+            const dataAtom = synctomic.buildAtom({ foo: { bar: ['baz'] } });
+
+            const watchSpy = sinon.spy();
+            dataAtom.onChange('foo.bar', watchSpy);
+
+            dataAtom.set((data, update) => {
+                data.foo.blerg = 'blerg';
+                update(data);
+            });
+
+            dataAtom.set((data, update) => {
+                data.foo.bar.push('quux');
+                update(data);
+            });
+
+            this.verify(prettyJson(watchSpy.args));
+
+        });
+
     });
 });
 
